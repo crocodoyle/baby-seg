713c185b84819158db9f2470fb9e9be0624e8f68
Andrew Doyle <doyle.andrew@gmail.com>
2017-04-21 12:07:31 -0400

    oops

--- train.py
+++ train.py
@@ -187,7 +187,7 @@ def batch(indices, class_weights=None):
             else:
                 label = to_categorical(labels[i, ...])
 
-            yield (images[i, ...][np.newaxis, ...], label[np.newaxis, ...], sample_weight)
+            yield (images[i, ...][np.newaxis, ...], label[np.newaxis, ...])
 
 
 if __name__ == "__main__":

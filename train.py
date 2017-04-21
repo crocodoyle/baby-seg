70a7f110b0fdc93dceb5529cc0ba773957819d81
Andrew Doyle <doyle.andrew@gmail.com>
2017-04-21 12:51:32 -0400

    removing temporal

--- train.py
+++ train.py
@@ -174,7 +174,7 @@ def batch(indices, class_weights=None):
             else:
                 label = to_categorical(labels[i, ...])
 
-            yield (images[i, ...][np.newaxis, ...], label.flatten()[np.newaxis, ...], sample_weight.flatten()[np.newaxis, ...])
+            yield (images[i, ...][np.newaxis, ...], label.flatten(), sample_weight.flatten())
 
 
 if __name__ == "__main__":

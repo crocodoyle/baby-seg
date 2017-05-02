9395114e9356a32e5c2d06054b86e2b46430e540
Andrew Doyle <doyle.andrew@gmail.com>
2017-05-02 15:36:20 -0400

    debug

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ class ConfusionCallback(Callback):
         predicted = model.predict(images[8,...][np.newaxis, ...], batch_size=1)
         segmentation = from_categorical(predicted, category_mapping).flatten()
 
-        conf = confusion_matrix(labels[8,...,0], segmentation)
+        conf = confusion_matrix(labels[8,...,0].flatten(), segmentation)
         print(conf)
 
         self.confusion.append(conf)

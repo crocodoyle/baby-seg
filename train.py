192947d9af5ddd5106e7eb4b7676daf41c5b720b
Andrew Doyle <doyle.andrew@gmail.com>
2017-05-03 12:48:55 -0400

    debug

--- train.py
+++ train.py
@@ -148,9 +148,9 @@ def segmentation_model():
 
     model = Model(input=[inputs], output=[conv14])
 
-    # model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
-    sgd = SGD(lr=0.0001, decay=1e-7, momentum=0.9, nesterov=True)
-    model.compile(optimizer=sgd, loss=dice_coef_loss, metrics=[dice_coef])
+    model.compile(optimizer=Adam(lr=1e-4), loss=dice_coef_loss, metrics=[dice_coef])
+    # sgd = SGD(lr=0.0001, decay=1e-7, momentum=0.9, nesterov=True)
+    # model.compile(optimizer=sgd, loss=dice_coef_loss, metrics=[dice_coef])
 
     return model
 

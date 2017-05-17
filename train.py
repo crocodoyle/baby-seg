545324f666622c4add2b612ba421f98a216c2e47
Andrew Doyle <doyle.andrew@gmail.com>
2017-05-17 14:45:10 -0400

    adding ratio img

--- train.py
+++ train.py
@@ -224,7 +224,7 @@ def segmentation_model():
 
     model = Model(input=[inputs], output=[conv14])
 
-    model.compile(optimizer=Adam(lr=1e-4, decay=1e-7), loss=dice_coef_loss, metrics=[dice_coef])
+    model.compile(optimizer=Adam(lr=1e-5, decay=1e-7), loss=dice_coef_loss, metrics=[dice_coef])
     # sgd = SGD(lr=0.0001, decay=1e-7, momentum=0.9, nesterov=True)
     # model.compile(optimizer=sgd, loss=dice_coef_loss, metrics=[dice_coef])
 
